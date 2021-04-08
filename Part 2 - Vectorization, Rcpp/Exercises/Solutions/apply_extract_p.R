9ac8c93b8cf95d032620b4efe4db423c4bff4580
Martin Schnuerch <martin.schnuerch@gmail.com>
2021-04-08 13:34:44 +0200

    updated Part II

--- Part 2 - Vectorization, Rcpp/Exercises/Solutions/apply_extract_p.R
+++ Part 2 - Vectorization, Rcpp/Exercises/Solutions/apply_extract_p.R
@@ -24,3 +24,53 @@ p <- unlist(p)
 
 # one step
 p <- sapply(res, `[[`, "p.value")
+
+# even better (when res becomes larger)
+p <- vapply(res, `[[`, 0, "p.value")
+
+
+#### The best solution
+
+# Once again, the best solution might be to look for a better alternative
+# We only want p-values, why use binom.test()?
+
+k <- rowSums(x)
+p <- pbinom(k - 1, ncol(x), .5, lower.tail = FALSE) # k - 1 because p(K > k)
+
+
+#### Some benchmarking
+
+results <- bench::mark(
+  apply = {
+    res <- apply(x, 1, new_binom_test, .5, "greater", .95)
+    p <- vapply(res, `[[`, 0, "p.value")
+  },
+  pbinom = {
+    k <- rowSums(x)
+    p <- pbinom(k - 1, ncol(x), .5, lower.tail = FALSE)
+  },
+  iterations = 100
+)
+
+results[, 1:8]
+
+results <- bench::press(
+  n = c(100, 1000),
+  {
+    x <- data.frame(matrix(rbinom(n * 100, 1, .5),
+                           ncol = n))
+    bench::mark(
+      apply = {
+        res <- apply(x, 1, new_binom_test, .5, "greater", .95)
+        p <- vapply(res, `[[`, 0, "p.value")
+      },
+      pbinom = {
+        k <- rowSums(x)
+        p <- pbinom(k - 1, ncol(x), .5, lower.tail = FALSE)
+      },
+      iterations = 100
+    )
+  }
+)
+
+results[, 1:8]
--- Part 2 - Vectorization, Rcpp/Exercises/Solutions/plyr_read multiple files.R
+++ Part 2 - Vectorization, Rcpp/Exercises/Solutions/plyr_read multiple files.R
@@ -1,3 +1,25 @@
 library(plyr)
 files <- list.files(path = "../Data/sprt/", pattern = "*.RData", full.names = TRUE)
-data <- ldply(files, function(X) get(load(x)), .progress = "text")
+data <- ldply(files, function(x) get(load(x)))
+
+
+##### An Alternative
+
+# There is actually an even more efficient way
+# Based on the purrr package 
+
+library(purrr)
+data <- map_df(files, function(x) get(load(x)))
+
+
+# Some Benchmarking
+
+results <- bench::mark(
+  plyr = ldply(files, function(x) get(load(x))),
+  purrr = map_df(files, function(x) get(load(x))),
+  iterations = 10
+)
+
+results[, 1:8]
+
+
